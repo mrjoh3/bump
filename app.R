90f7d3bd396a7222fc88da0d793961280f68bc47
Matt Johnson <matthew.johnson@epa.vic.gov.au>
2020-12-03 10:10:06 +1100

    minimal UI

--- app.R
+++ app.R
@@ -1,49 +1,56 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+library(shiny.semantic)
+ui <- semanticPage(
+    title = "Bump",
+    flow_layout(row_gap = "30px", cell_width = '100%',
+        div(
+          h1('Bump Me'),
+          p('Do you remember what is was like to bump into someone in the office? ',
+            'To see a familiar face walking from the train station or waiting for a coffee?'
+            )
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+      split_layout(cell_widths = c("15%", "50%", "20%", "15%"), cell_args = "padding: 10px;",
+                   div(),
+                   segment(
+                       form(
+                           h4(class = "ui dividing header", "Inputs"),
+                           field(
+                               tags$label("First Name"),
+                               text_input("name_first", value = "", type = "text", placeholder = "Enter Text...")
+                           ),
+                           field(
+                               tags$label("Last Name"),
+                               text_input(
+                                   "name_last", value = "", type = "text", placeholder = "Enter Text...")
+                           ),
+                           field(
+                               tags$label("E-Mail"),
+                               text_input("email_ex", value = "", type = "email", placeholder = "Enter E-Mail")
+                           ),
+                           field(
+                               tags$label("Options"),
+                               multiple_checkbox(
+                                   "preferences", "", choices = c('Coffee', 'Chat', 'Lunch', 'Walk'), position = "inline"
+                               )
+                           ),
+                           field(
+                               #tags$label(),
+                               action_button('submit', 'BUMP', width = '100%')
+                           ))),
+                   div(
+                       h3('Instructions'),
+                       p('loads of text')
+                   ),
+                   div()
+      ),
+      div(p('output area'))
     )
+
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output) {}
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+
+shinyApp(ui, server)
